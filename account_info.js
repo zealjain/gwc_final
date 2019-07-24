b69a8ab7da217bba77f9162dda126f8719d93672
Zeal Jain <zeal.jain110@gmail.com>
2019-07-24 15:56:51 -0700

    Incorporting Profile logo into nav bar

--- account_info.js
+++ account_info.js
@@ -1,12 +1,21 @@
 function incorrect() {
   document.getElementById("after_submit").style.visibility = "visible";
 };
+
+var signin = 0;
 function check() {
   var email = document.form.email.value;
   var password = document.form.psw.value;
   if (email == "zeal.jain110@gmail.com" && password == "hello") {
+    signin = 1;
     window.location.href = "account.html";
   } else {
     document.getElementById("after_submit").style.visibility = "visible";
   }
+}
+
+if (signin == 0) {
+  document.getElementById("profile") = "Sign in";
+} else {
+  document.getElementById("profile") = "<img src='profile.png' alt='Profile Logo' width=40px height=40px >";
 }
