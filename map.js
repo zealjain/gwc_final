2fb03ad79a951f2f416af55028b4721dfc5f07df
Julianne <julianneychen@gmail.com>
2019-07-24 14:30:03 -0700

    added data to map

--- map.js
+++ map.js
@@ -12,9 +12,11 @@ function initMap() {
     zoom: 9,
     mapTypeId: 'roadmap'
   });
+
+  createMarker(37.381388, -122.069821, "Walnut", "Unkown", "n/a", "n/a", "Tree is large and unmanaged, right next to gas station.", "www.fallingfruit.com")
   createMarker(37.363528, -122.042417, "Orange", "Private", "n/a", "n/a", "Tree located on private property but overhangs to sidewalk. Ask owner for permission to pick but lots of fruit on ground.", "www.fallingfruit.com")
   createMarker(37.32675000, -122.062954, "Apricot", "Public", "n/a", "n/a", "Multiple trees located in Varian Park, open to public. Good yeild.", "user")
-  createMarker(37.378335, -122.028976, "Avocado", "Unknown", "n/a", "n/a", "Tree overhangs fence on Caltrain", "www.fallingfruit.com")
+  createMarker(37.378335, -122.028976, "Avocado", "Public", "n/a", "n/a", "Tree overhangs Caltrain fence to public area", "www.fallingfruit.com")
 
   initAutocomplete();
 }
