b6eac126248fc108122f2e6d3b1be5e980277473
Dhriti <goomer.dhriti@gmail.com>
2019-07-25 15:29:59 -0700

    created id for everything and variables

--- post.js
+++ post.js
@@ -6,13 +6,21 @@ function postBackgroundColor() {
 
 
 function mapData() {
-  var address = document.getElementByName('finnaPost');
+  var address = document.getElementbyId('addressFruit');
+  var fruitType = document.getElementbyId('fruitType');
+  var latitude = document.getElementbyId('addressFruit');
+  var longitude document.getElementbyId('addressFruit');
+  var phoneNum =document.getElementbyId('phoneNum');
+  var email = document.getElementbyId('email');
+  var instructions = document.getElementbyId('notes');
+
+
   var fruitType = document.getElementByName('fruitType');
   alert(fruitType);
   address.innerHTML = "Welcome " +fruitType.value + ".<br/>";
   alert(address.innerHTML);
 
   // form.Content.innerHTML = "";
-
+latitude, longitude, fruitType, accessibility, phoneNum, email, instructions, source
 
 }
