aedd6e9b4cf2b31afd62cadfc2abd870ba0d8123
Zeal Jain <zeal.jain110@gmail.com>
2019-07-25 16:01:19 -0700

    working on implenting json into javascript

--- account_info.js
+++ account_info.js
@@ -4,47 +4,53 @@ function incorrect() {
 }
 var signin = 0;
 function check() {
-  var email = document.form.email.value;
-  var password = document.form.psw.value;
-  if (email == "zeal.jain110@gmail.com" && password == "hello") {
-    signin = 1;
-    if (signin == 1) {
-      alert(signin);
+  fetchJSON('fruitful.json') {
+    for (var i = 0; i <fruitful.accounts.length; i++) {
+      var email = document.form.email.value;
+      var password = document.form.psw.value;
+      if (email == fruitful.accounts.i.email && password == fruitful.accounts.i.password) {
+        var correct = 1;
+      }
     }
-    window.location.href = "account.html";
-  } else {
-    document.getElementById("after_submit").style.visibility = "visible";
-  }
+    if (correct == 1) {
+      fruitful.accounts.i.status = "in";
+      window.location.href = "account.html";
+    } else {
+      document.getElementById("after_submit").style.visibility = "visible";
+    }
 }
 
 
 function signout() {
-  signin = 0;
-  window.location.href = "profile.html";
+  fetchJSON('fruitful.json') {
+    fruitufl.accounts. = 0;
+    window.location.href = "profile.html";
 }
 
 window.onload = function init() {
-  if (signin == 0) {
-    alert("lol");
-    document.getElementById("lol").innerHTML = "<a href='profile.html'>Sign in</a>";
-  } else if (signin == 1) {
-    alert("ok");
-    document.getElementById('lol').innerHTML = "<a href='profile.html'><img src='profile.png' alt='Profile Logo' width=40px height=40px ></a>";
+  fetchJSON("fruitful.json") {
+    if (fruitful.accounts.i.status == "out") {
+      alert("lol");
+      document.getElementById("lol").innerHTML = "<a href='profile.html'>Sign in</a>";
+    } else {
+      alert("ok");
+      document.getElementById('lol').innerHTML = "<a href='profile.html'><img src='profile.png' alt='Profile Logo' width=40px height=40px ></a>";
+    }
   }
 }
 
 
 
-// function fetchJSON(path, callback) {
-//   var httpRequest = new XMLHttpRequest();
-//   httpRequest.onreadystatechange = function() {
-//     if (httpRequest.readyState === 4) {
-//       if (httpRequest.status === 200) {
-//         var data = JSON.parse(httpRequest.responseText);
-//         if (callback) callback(data);
-//       }
-//     }
-//   };
-//   httpRequest.open('GET', path);
-//   httpRequest.send();
-// }
+function fetchJSON(path, callback) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        var data = JSON.parse(httpRequest.responseText);
+        if (callback) callback(data);
+      }
+    }
+  };
+  httpRequest.open('GET', path);
+  httpRequest.send();
+}
