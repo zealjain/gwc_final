d100dbcbb0a754b232f5b14ceb5ddf8ba545472c
Dhriti <goomer.dhriti@gmail.com>
2019-07-25 14:22:49 -0700

    fruit type input testing

--- post.js
+++ post.js
@@ -6,7 +6,12 @@ function postBackgroundColor() {
 
 
 function mapData() {
-  var address = document.getElementById('addressFruit')
+  var address = document.getElementById('addressFruit');
+  var fruitType = document.getElementById('fruitType');
   alert(address);
+  alert(fruitType);
+
+  address.innerHTML = "Welcome " +fruitType.value + ".<br/>";
+  form.Content.innerHTML = "";
 
 }
