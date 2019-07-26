d26b5338df6ddcdebb58ba29714041cc1ddb6f1b
Julianne <julianneychen@gmail.com>
2019-07-26 15:48:35 -0700

    started geocoding

--- post.js
+++ post.js
@@ -1,6 +1,17 @@
+geocoder = new google.maps.Geocoder();
+
 function postBackgroundColor() {
   document.body.style.backgroundColor = "#F5E1BD";
 
+}
+
+function getCoordinates(address, callback){
+  var coordinates;
+  geocoder.geocode({address: address}, function (results, status){
+    coordinatesObj = results[0].geometry.location;
+    coordinates = [coordinatesObj.nb, coordinatesObj.ob];
+    callback(coordinates);
+  })
 }
 
 
