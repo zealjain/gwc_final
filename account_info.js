ab700d1c1b7642e2a15c800f3961bb1946ff3562
zealjain <zeal.jain110@gmail.com>
2019-07-25 22:20:29 -0700

    adding json

--- account_info.js
+++ account_info.js
@@ -1,56 +1,65 @@
 
+function fetchJSON(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send();
+}
 function incorrect() {
   document.getElementById("after_submit").style.visibility = "visible";
 }
 var signin = 0;
 function check() {
-  fetchJSON('fruitful.json') {
+  fetchJSON('fruitful.json', function(fruitful) {
     for (var i = 0; i <fruitful.accounts.length; i++) {
       var email = document.form.email.value;
       var password = document.form.psw.value;
-      if (email == fruitful.accounts.i.email && password == fruitful.accounts.i.password) {
+      if (email == fruitful.accounts[i].email && password == fruitful.accounts[i].password) {
         var correct = 1;
+        var account_index = i;
       }
     }
     if (correct == 1) {
-      fruitful.accounts.i.status = "in";
+      fruitful.accounts[account_index].status = "in";
       window.location.href = "account.html";
     } else {
       document.getElementById("after_submit").style.visibility = "visible";
     }
+  });
+  fetchJSON('loggedin.json',function(loggedin) {
+    person[0] = account_index;
+  });
 }
 
 
 function signout() {
-  fetchJSON('fruitful.json') {
-    fruitufl.accounts. = 0;
-    window.location.href = "profile.html";
+  fetchJSON('fruitful.json', function(fruitful) {
+    fetchJSON('loggedin.json', function(loggedin) {
+      var account_index = person[0];
+      fruituful.accounts[account_index].status = 'out';
+      window.location.href = "profile.html";
+    });
+  });
 }
 
 window.onload = function init() {
-  fetchJSON("fruitful.json") {
-    if (fruitful.accounts.i.status == "out") {
-      alert("lol");
-      document.getElementById("lol").innerHTML = "<a href='profile.html'>Sign in</a>";
-    } else {
-      alert("ok");
-      document.getElementById('lol').innerHTML = "<a href='profile.html'><img src='profile.png' alt='Profile Logo' width=40px height=40px ></a>";
-    }
-  }
-}
-
-
-
-function fetchJSON(path, callback) {
-  var httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = function() {
-    if (httpRequest.readyState === 4) {
-      if (httpRequest.status === 200) {
-        var data = JSON.parse(httpRequest.responseText);
-        if (callback) callback(data);
+  fetchJSON("fruitful.json", function(fruitful) {
+    fetchJSON("loggedin.json", function(loggedin) {
+      var account_index = person[0];
+      if (fruitful.accounts[account_index].status == "out") {
+        alert("lol");
+        document.getElementById("lol").innerHTML = "<a href='profile.html'>Sign in</a>";
+      } else {
+        alert("ok");
+        document.getElementById('lol').innerHTML = "<a href='profile.html'><img src='profile.png' alt='Profile Logo' width=40px height=40px ></a>";
       }
-    }
-  };
-  httpRequest.open('GET', path);
-  httpRequest.send();
+    });
+  });
 }
