2ac9b56094e366f1acd0713ff7cfcade109f8e03
Dhriti <goomer.dhriti@gmail.com>
2019-07-26 15:38:51 -0700

    fixed the problem on js and html post

--- post.js
+++ post.js
@@ -1,3 +1,6 @@
+function sayhi() {
+  alert("pickles");
+}
 function postBackgroundColor() {
   document.body.style.backgroundColor = "#F5E1BD";
 
@@ -6,8 +9,8 @@ function postBackgroundColor() {
 
 
 function mapData() {
-  var latitude = document.getElementbyId('addressFruit');
-  var longitude document.getElementbyId('addressFruit');
+  // var latitude = document.getElementbyId('addressFruit');
+  // var longitude document.getElementbyId('addressFruit');
   var fruitType = document.getElementbyId('fruitType');
   var accesibility = document.getElementbyId9('accessibility');
   var phoneNum =document.getElementbyId('phoneNum');
@@ -16,8 +19,4 @@ function mapData() {
   var source = document.getElementbyId('source');
   // alert("Okay! All your data has been entered press 'Continue'")
   // form.Content.innerHTML = "";
-
-}
-function sayhi() {
-  alert("pickles");
 }
