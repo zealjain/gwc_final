b46d3115df299ffaa4b6edb012ca6d6c2a6a8200
Dhriti <goomer.dhriti@gmail.com>
2019-07-26 14:08:14 -0700

    added variables to js

--- post.js
+++ post.js
@@ -5,15 +5,19 @@ function postBackgroundColor() {
 
 
 
-function createMarker() {
-  var address = document.getElementbyId('addressFruit');
-  var fruitType = document.getElementbyId('fruitType');
+function mapData() {
   var latitude = document.getElementbyId('addressFruit');
   var longitude document.getElementbyId('addressFruit');
+  var fruitType = document.getElementbyId('fruitType');
+  var accesibility = document.getElementbyId9('accessibility');
   var phoneNum =document.getElementbyId('phoneNum');
   var email = document.getElementbyId('email');
   var instructions = document.getElementbyId('notes');
+  var source = document.getElementbyId('source');
   // alert("Okay! All your data has been entered press 'Continue'")
   // form.Content.innerHTML = "";
 
 }
+lat long
+
+fruit accessibility phone number email speical instruction source
