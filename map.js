36503cf0c99727b8a381947766cc1a9416ca5916
Julianne <julianneychen@gmail.com>
2019-07-29 13:44:44 -0700

    changed geocoding

--- map.js
+++ map.js
@@ -97,7 +97,7 @@ function initMarkers(){
   createMarker(37.395110, -122.057683, "Apricot", "Private", "n/a", "n/a", "Huge abandoned orchard of apricots, behind Huawei and MobileIron parking lot. Lots of fruit from June-July.", "www.fallingfruit.com");
   createMarker(37.348603, -122.03547, "Macadamia tree", "Private", "n/a", "n/a", "Huge abandoned orchard of apricots, behind Huawei and MobileIron parking lot. Lots of fruit from June-July.", "www.fallingfruit.com");
   createMarker(37.348581, -122.035824, "Lemon", "Private", "n/a", "n/a", "Tree in private backyard but sometimes fruit falls on ground or overhangs backyard.", "www.fallingfruit.com");
-  createMarker(37.321622, -122.037965, "Black Mulberry", "Private", "n/a", "n/a", "Behind Staples parking lot, fruit overhangs from backyard.", "www.fallingfruit.com");
+  createMarker(37.321622, -122.037965, "Black Mulberry", "Private", "n/a", "n/a", "Behind Staples parking lot, fruit overhangs from backyard.", "www.fallingfruit.org");
 }
 
 //Convert user input address into lat, long
