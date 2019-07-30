86420ba7e8a4fae803294518ae949e1dc1bb9b7b
zealjain <zeal.jain110@gmail.com>
2019-07-30 09:28:03 -0700

    connecting database

--- account_creation.php
+++ account_creation.php
@@ -1,5 +1,8 @@
 <?php
   if (isset($_POST["signup-submit"])) {
+
+    require 'dbh.php';
+
      $username = $_POST['uid'];
      $name = $_POST['fname'];
      $lname = $_POST['lname'];
@@ -8,32 +11,32 @@
      $password = $_POST['pwd'];
      $password_repeat = $_POST['pwd-repeat'];
 
-     if (empty($username) || (empty($name) || (empty($lname) || (empty($email) || (empty($address)) {
-       header("Location: ../signup.php?error=emptyfields&uid=".$username."&fname".$name."&lname".$lname."&address".$address."&email=".$email);
+     if (empty($username) || (empty($name) || (empty($lname) || (empty($email)) {
+       header("Location: ../signup_form.html?error=emptyfields&uid=".$username."&fname".$name."&lname".$lname."&address".$address."&email=".$email);
        exit();
      }
      else if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !prep_match("/^[a-zA-Z0-9]*$/", $username)) {
-       header("Location: ../signup.php?error=invalidmailuid".);
+       header("Location: ../signup_form.html?error=invalidmailuid".);
        exit();
      }
      else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-       header("Location: ../signup.php?error=invalidmail&uid=".$username);
+       header("Location: ../signup_form.html?error=invalidmail&uid=".$username);
        exit();
      }
      else if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-       header("Location: ../signup.php?error=invaliduid&mail=".$username);
+       header("Location: ../signup_form.html?error=invaliduid&mail=".$username);
        exit();
      }
      else if ($password !== $password_repeat)) {
-       header("Location: ../signup.php?error=passwordcheck&mail=".$email);
+       header("Location: ../signup_form.html?error=passwordcheck&mail=".$email);
        exit();
      }
      else {
 
-       $sql = "SELECT uidUsers FROM users WHERE uidUsers=? AND pwdUsers="
+       $sql = "SELECT uid FROM id WHERE uid=? AND pwd="
        $stmt = mysqli_stmt_init($conn);
        if (!mysqli_stmt_prepare($stmt, $sql)) {
-         header("Location: ../signup.php?error=sqlerror");
+         header("Location: ../signup_form.html?error=sqlerror");
          exit();
        }
        else {
@@ -42,49 +45,30 @@
          mysqli_stmt_store_result($stmt);
          $resultCheck = mysqli_stmt_num_rows();
          if ($resultCheck > 0) {
-           header("Location: ../signup.php?error=usertaken&email=".$email);
+           header("Location: ../signup_form.html?error=usertaken&email=".$email);
            exit();
          }
          else {
-           $sql = "INSERT INTO users () VALUES ()";
+           $sql = "INSERT INTO id (uid, email, pwd, fname, lname) VALUES (?,?,?)";
+           $stmt = mysqli_stmt_init($conn);
+           if (!mysqli_stmt_prepare($stmt, $sql)) {
+             header("Location: ../signup_form.html?error=sqlerror");
+             exit();
+         }
+         else {
+           $hashedPwd = password_hash($password, PASSWORD_DEFAULT)
+           mysqli_stmt_bind_param($stmt, "sssss", $username, $email, $password, $name, $lname);
+           mysqli_stmt_execute($stmt);
+           header("Location: ../signupup_form.html?signup=success");
+           exit();
          }
        }
-       // $userformdata = array(
-       //   'uid' => $username,
-       //   'name' => $name,
-       //   'lname' => $lname,
-       //   'address' => $address,
-       //   'email' => $email,
-       //   'pwd' => $password
-       //  );
-       //
-       // //user data file name
-       //  $userFile = "./userdata.json";
-       //
-       //  //load local Json get_included_file
-       //  $jsonuserdata = file_get_contents($userFile) ;
-       //
-       //   //Push newly added user in to array
-       //  array_push($user_arr_data, $userformdata) ;
-       //
-       //
-       //
-       // //Convert updated arrow in to Jason
-       // $jsonuserdata = json_encode($userformdata, JSON_PRETTY_PRINT) ;
-       //
-       //
-       // //Write json data in to userdata.json
-       //
-       // if(file_put_contents($userFile, $jsonuserdata)){
-       //    header("Location: ../signup.php?signup=success")
-       // }else {
-       //   echo "error daving data" ;
-       // }
      }
-     // LEARN HOW TO LOOP THROUGH JSON FILE TO CHECK IF USERNAME IS TAKEN
-     // else {
-     //
-     // }
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
   }
-
+  else {
+    header("Location: ..signup_form.html")
+  }
 ?>
