a8055563a9aeae56dfeee4c2d58d492c098e2968
Julianne <julianneychen@gmail.com>
2019-07-30 13:19:32 -0700

    formatted lines

--- post.js
+++ post.js
@@ -30,41 +30,25 @@ function getPosition() {
 }
 
 function mapData() {
-  alert("map data running")
-  //var address = document.getElementbyId('addressFruit').innerHTML;
-  // var longitude document.getElementbyId('addressFruit').value;
   var tempPosition = getPosition();
-  var latitude = tempPosition.lat();
-  var longitude = tempPosition.lng();
-  alert(latitude + "," + longitude);
-
-  //fruit
+  var lat = tempPosition.lat();
+  var lng = tempPosition.lng();
   var fruitType = document.getElementById("fruitType").value;
-  alert(fruitType);
-
-  //accessibility
-  var accesibility = document.getElementById("accessibility").value;
-  alert(accesibility);
-
-  //phone Number
+  var accessibility = document.getElementById("accessibility").value;
   var phoneNum = document.getElementById("phoneNum").value;
-  alert(phoneNum);
-
-  //email
   var email = document.getElementById("email").value;
-  alert(email);
-
   var instructions = document.getElementById("notes").value;
-  alert(instructions);
-
   var source = document.getElementById("source").value;
-  alert(source);
 
-  getConfirmation();
+  var confirm = getConfirmation();
+  if(confirm == true){
+    createMarker(lat, lng, fruitType, accessibility, phoneNum, email, instructions, source);
+    window.location.href = 'pick.html';
+  }
 
 }
 
 function getConfirmation() {
-  var response = confirm("Is all data correct? Press OK to Continue");
+  var response = confirm("By pressing OK you are agreeing to our Terms and Conditions");
   return response;
 }
