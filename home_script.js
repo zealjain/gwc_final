4dd24b335b009371ced364463ea5e12e3e1cf417
Michelle <Michellew94086@gmail.com>
2019-08-10 22:48:26 -0700

    added hamburger menu for screen sizes less than 1100px wide

--- home_script.js
+++ home_script.js
@@ -1,7 +1,7 @@
 
 function hamburger_toggle() {
-  const burger_toggle = document.querySelectorAll('#myLinks');
-  if (burger_toggle.style.display == "block") {
+  const burger_toggle = document.getElementById('myLinks');
+  if (burger_toggle.style.display === "block") {
     burger_toggle.style.display = "none";
   } else {
     burger_toggle.style.display = "block";
