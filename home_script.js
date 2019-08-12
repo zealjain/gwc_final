52123fa5b4f8d7a6284b2c835e0f10d16eb02096
Michelle <Michellew94086@gmail.com>
2019-08-11 23:28:45 -0700

    ADDED HAMBURGER MENU :D
    still working on it!! JQuery currently not cooperating but ill get it... eventually

--- home_script.js
+++ home_script.js
@@ -1,9 +1,14 @@
 
-function hamburger_toggle() {
-  const burger_toggle = document.getElementById('myLinks');
-  if (burger_toggle.style.display === "block") {
-    burger_toggle.style.display = "none";
-  } else {
-    burger_toggle.style.display = "block";
-  }
-}
+$('.menu-toggle').click(function() {
+  alert("works")
+  $('.site-nav').toggleClass('site-nav--open');
+})
+
+// function hamburger_toggle() {
+//   const burger_toggle = document.getElementById('myLinks');
+//   if (burger_toggle.style.display === "block") {
+//     burger_toggle.style.display = "none";
+//   } else {
+//     burger_toggle.style.display = "block";
+//   }
+// }
