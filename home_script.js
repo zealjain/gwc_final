87223b0c919dc12e22695ea8de9e7adba0a77b5f
Michelle <Michellew94086@gmail.com>
2019-08-14 15:05:04 -0700

    ADDED HAMBURGER MENU :D
    Sign in page is still a lil weird will fix later

--- home_script.js
+++ home_script.js
@@ -1,14 +1,4 @@
 
 $('.menu-toggle').click(function() {
-  alert("works")
-  $('.site-nav').toggleClass('site-nav--open');
+  $('.site-nav').toggleClass('site-nav--open', 500);
 })
-
-// function hamburger_toggle() {
-//   const burger_toggle = document.getElementById('myLinks');
-//   if (burger_toggle.style.display === "block") {
-//     burger_toggle.style.display = "none";
-//   } else {
-//     burger_toggle.style.display = "block";
-//   }
-// }
